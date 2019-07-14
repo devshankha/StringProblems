1bb4c9a98388cbb22d9f6cfc92f640e0818a19dd
devshankha <45039485+devshankha@users.noreply.github.com>
2019-07-14 16:51:28 +0530

    Update Permutations.java

--- Permutations.java
+++ Permutations.java
@@ -1,44 +1,38 @@
-//this code is used to generate permutations of a string
-//code has been taken from -https://www.youtube.com/watch?v=cQw8h_prIxc
 import java.util.*;
+
+//Program to print all permutations of
+//a string, logic best understood by drawing recursive tree
 
 public class Permutations {
 	public static void main(String[] args) {
-		String word = "de";
-		List<Integer> indices = new ArrayList<Integer>();
-		List<String> permutations = Permutations.getPermutations(word, indices);
-		System.out.println(permutations);
+		String s = "abc"; 
+        printPermutn(s, "");
 
 	}
 
-	public static List<String> getPermutations(String word, List<Integer> indices) {
-		int length = word.length();
-		List<String> toReturn = new ArrayList<String>();
-		if (indices.size() == length) {
-			toReturn.add("");
-			return toReturn;
-		}
-		// Iterate over each character to find permutation
-		for (int i = 0; i < length; i++) {
-			// skip if index is already considered
-			if (indices.contains(i))
-				continue;
+	// Function to print all the permutations of str
+	static void printPermutn(String str, String ans) 
+    { 
+  
+        // If string is empty 
+        if (str.length() == 0) { 
+            System.out.print(ans + " "); 
+            return; 
+        } 
+  
+        for (int i = 0; i < str.length(); i++) { 
+  
+            // ith character of str 
+            char ch = str.charAt(i); 
+  
+            // Rest of the string after excluding  
+            // the ith character 
+            String ros = str.substring(0, i) +  
+                         str.substring(i + 1); 
+  
+            // Recurvise call 
+            printPermutn(ros, ans + ch); 
+        } 
+    } 
 
-			// Create and populate a duplicare arraylist of indices that have already
-			// been populated
-			List<Integer> newIndices = new ArrayList<Integer>();
-			for (int index : indices)
-				newIndices.add(index);
-			// Add current index
-			newIndices.add(i);
-
-			// Calculate the permutations
-			List<String> permutations = Permutations.getPermutations(word, newIndices);
-			for (String str : permutations)
-				toReturn.add(word.charAt(i) + str);
-
-		}
-		return toReturn;
-
-	}
 }
