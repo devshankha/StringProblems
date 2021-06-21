437033d414d83a4114f5a68065547b8acc23086c
devshankha <45039485+devshankha@users.noreply.github.com>
2021-06-21 17:21:06 +0530

    Update AreAnagrams.java

--- AreAnagrams.java
+++ AreAnagrams.java
@@ -13,6 +13,26 @@ public class AreAnagrams {
 		return val == 0;
 	}
 	
+	// another solution to the problem
+	//convert the strings into character arrays
+	//sort them and compare the two arrays
+	
+	static boolean areAnagranms(String s1, String s2) {
+		if (s1.length() != s2.length())
+			return false;
+		char[] a = s1.toCharArray();
+		char[] b = s2.toCharArray();
+		Arrays.sort(a);
+		Arrays.sort(b);
+		System.out.println(Arrays.toString(a));
+		System.out.println(Arrays.toString(b));
+		for (int i=0; i < a.length;i++) {
+			if (a[i] != b[i])
+				return false;
+		}
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println(areAnagrams("Kamal","mKlaa"));
 		System.out.println(areAnagrams("Kamal1","mKlaa"));
